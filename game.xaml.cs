1741a51ff62713a98b77d47c1be45f750d2d631e
jdombrowski <jdombrowski@S3004>
2025-10-01 13:07:17 +0200

    s

--- game.xaml.cs
+++ game.xaml.cs
@@ -20,14 +20,23 @@ namespace Saper
     /// </summary>
     public partial class game : Page
     {
+        public int dificulty;
+        public bool firstMove;
+        public int height;
+        public int width;
         public game()
         {
             InitializeComponent();
         }
-        public List<List<Label>> labels = new List<List<Label>>();
-        public game(int width, int height, int dificulty) {
+        public List<List<Button>> buttons = new List<List<Button>>();
+        public List<List<int>> values = new List<List<int>>();
+        public game(int width, int height, int dif) {
             InitializeComponent();
             gameGrid.Children.Clear();
+            dificulty = dif;
+            firstMove = true;
+            this.height = height;
+            this.width = width;
             for (int x = 0; x < width; x++)
             {
                 ColumnDefinition col = new ColumnDefinition();
@@ -43,21 +52,77 @@ namespace Saper
             }
             for (int x = 0; x < width; x++)
             {
-                labels.Add(new List<Label>());
+                buttons.Add(new List<Button>());
+                values.Add(new List<int>());
                 for (int y = 0; y < height; y++)
                 {
-                    Label label = new Label();
-                    label.Content = "r" + x + "c" + y;
+                    Button label = new Button();
+                    values[x].Add(0);
                     gameGrid.Children.Add(label);
                     Grid.SetRow(label, y);
                     Grid.SetColumn(label, x);
-                    labels[x].Add(label);
+                    buttons[x].Add(label);
+                    label.Click += (s,e)=>GenerateBombs(s, e);
+                }
+            }
+            showValues();
+        }
+        private void showValues()
+        {
+            for (int x = 0; x < width; x++)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    buttons[x][y].Content = "r" + x + "c" + y + "v" + values[x][y];
                 }
             }
         }
-        public int dificulty;
+        private void GenerateBombs(object sender, RoutedEventArgs e)
+        {
+            int clickedX = -1, clickedY = -1, numberOfBombs, divident;
+            Random rand = new Random();
+            for (int x = 0; x < width; x++)
+                for (int y = 0; y < height; y++)
+                    if (buttons[x][y] == sender) {
+                        clickedX = x;
+                        clickedY = y;   
+                    }
+            if (clickedX < 0 || clickedY < 0) {
+                return;
+            }
+            List<List<int>> freeFields = new List<List<int>>();
+            for(int x = 0; x < width; x++)
+                for(int y = 0; y < height; y++)
+                    if (x != clickedX && x != clickedX - 1 && x != clickedX + 1 && y != clickedY && y != clickedY - 1 && y != clickedY + 1)
+                        freeFields.Add(new List<int> { x, y });
+            switch (dificulty)
+            {
+                case 1:
+                    divident = 4;
+                    break;
+                case 2:
+                    divident = 3;
+                    break;
+                case 3:
+                    divident = 2;
+                    break;
+                default:
+                    divident = 5;
+                    break;
 
-        private void startNewGame(object sender, MouseButtonEventArgs e)
+            }
+            numberOfBombs = width * height / 2;
+            if (numberOfBombs > freeFields.Count)
+                numberOfBombs = freeFields.Count;
+            for(int i = 0;i < width; i++)
+            {
+                List<int> currentCoordinates = freeFields[rand.Next() % freeFields.Count];
+                values[currentCoordinates[0]][currentCoordinates[1]] = -1;
+            }
+            showValues();
+        }
+
+        private void StartNewGame(object sender, MouseButtonEventArgs e)
         {
             NavigationService.GoBack();
         }
